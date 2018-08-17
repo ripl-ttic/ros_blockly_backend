ab5b564b604e970ce96d85e6658b1049a001dce1
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-17 18:01:01 -0500

    docker image moved to another repo

--- docker/build-all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t afdaniele/rpi-ros-blockly-backend:latest ./arm32v7/
--- docker/deploy-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./build-all.sh
-
-if [ $? -eq 0 ]; then
-    docker push afdaniele/rpi-ros-blockly-backend:latest
-fi
